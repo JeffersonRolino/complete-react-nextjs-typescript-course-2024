d27361a78bad503fc38ba17083152103826b5dd5
jeffersonrolino <jefferson.rolino@gmail.com>
2024-06-27 16:55:48 -0300

    Lesson 33: Simple List

--- 03-fundamentals/src/index.js
+++ 03-fundamentals/src/index.js
@@ -2,17 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-const firstBook = {
-    title: "A Game of Thrones",
-    author: "George R. R. Martin",
-    image: "./images/a-game-of-thrones.jpg",
-};
-
-const secondBook = {
-    title: "A Clash of Kings",
-    author: "George R. R. Martin",
-    image: "./images/a-clash-of-kings.jpg",
-};
+const books = [
+    {
+        title: "A Game of Thrones",
+        author: "George R. R. Martin",
+        image: "./images/a-game-of-thrones.jpg",
+    },
+    {
+        title: "A Clash of Kings",
+        author: "George R. R. Martin",
+        image: "./images/a-clash-of-kings.jpg",
+    },
+];
 
 // BookList Component
 const BookList = () => {
@@ -20,31 +21,23 @@ const BookList = () => {
         <div className="container">
             <h1>Fantasy Books</h1>
             <section className="book-list">
-                <Book
-                    image={firstBook.image}
-                    title={firstBook.title}
-                    author={firstBook.author}
-                >
-                    <button>Buy Now</button>
-                </Book>
-                <Book
-                    image={secondBook.image}
-                    title={secondBook.title}
-                    author={secondBook.author}
-                />
+                {names.map((name) => {
+                    return <h2>{name}</h2>;
+                })}
             </section>
         </div>
     );
 };
 
+const names = ["John", "Peter", "Mary", "Tony"];
+
 // Book Component
-function Book({ image, title, author, children }) {
+function Book({ image, title, author }) {
     return (
         <article className="book">
             <img src={image} alt="book cover" />
             <h2>{title}</h2>
             <h4>{author}</h4>
-            {children}
         </article>
     );
 }
