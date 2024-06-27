06cd173fe27658f87ee0109947a7ec479924a2c8
jeffersonrolino <jefferson.rolino@gmail.com>
2024-06-27 11:41:51 -0300

    Lesson 21: JSX Rules

--- 03-fundamentals/src/index.js
+++ 03-fundamentals/src/index.js
@@ -2,10 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function Greeting() {
-    return React.createElement(
-        "div",
-        {},
-        React.createElement("h2", {}, "Hello World!")
+    return (
+        //React Fragment
+        //Atributes follow camel case sintax
+        <>
+            <h1 className="title">Hello World</h1>
+            <div tabIndex={1}>
+                <ul>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">About Us</a>
+                    </li>
+                </ul>
+            </div>
+        </>
+        // Cannot return more than one Element
+        // <h2>This return a error!</h2>
     );
 }
 
