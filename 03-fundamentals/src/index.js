df08b0434a0bb641d791d5ee0bb1fc4768642fe1
jeffersonrolino <jefferson.rolino@gmail.com>
2024-06-27 12:19:33 -0300

    Lesson 24: BookList

--- 03-fundamentals/src/index.js
+++ 03-fundamentals/src/index.js
@@ -1,17 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Person = () => <h2>John Doe</h2>;
-const Message = () => <p>This is my Message...</p>;
-
-function Greeting() {
+const BookList = () => {
     return (
-        <div>
-            <Person />
-            <Message />
-        </div>
+        <section>
+            <h1>Books</h1>
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+        </section>
+    );
+};
+
+const Image = () => (
+    <img
+        src="http://georgerrmartin.com/gallery/coverart/GOThcEng.jpg"
+        alt="Game of Thrones book cover"
+    ></img>
+);
+const Title = () => <h2>A Game of Thrones</h2>;
+const Author = () => <h4>George R. R. Martin</h4>;
+
+function Book() {
+    return (
+        <article>
+            <Image />
+            <Title />
+            <Author />
+        </article>
     );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Greeting />);
+root.render(<BookList />);
