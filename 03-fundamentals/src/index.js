e3b615f3cd20eb414fbd270b51c3ff98467849ed
jeffersonrolino <jefferson.rolino@gmail.com>
2024-06-27 11:47:42 -0300

    Lesson 22: Nest Components

--- 03-fundamentals/src/index.js
+++ 03-fundamentals/src/index.js
@@ -1,25 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const Person = () => <h2>John Doe</h2>;
+const Message = () => <p>This is my Message...</p>;
+
 function Greeting() {
     return (
-        //React Fragment
-        //Atributes follow camel case sintax
-        <>
-            <h1 className="title">Hello World</h1>
-            <div tabIndex={1}>
-                <ul>
-                    <li>
-                        <a href="#">Home</a>
-                    </li>
-                    <li>
-                        <a href="#">About Us</a>
-                    </li>
-                </ul>
-            </div>
-        </>
-        // Cannot return more than one Element
-        // <h2>This return a error!</h2>
+        <div>
+            <Person />
+            <Message />
+        </div>
     );
 }
 
