9e89402cae4ed8781598cd0fe0b9b8337432063f
jeffersonrolino <jefferson.rolino@gmail.com>
2024-06-27 15:55:47 -0300

    Lesson 27: JSX - Inline CSS

--- 03-fundamentals/src/index.js
+++ 03-fundamentals/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
+// BookList Component
 const BookList = () => {
     return (
         <div className="container">
@@ -18,15 +19,25 @@ const BookList = () => {
     );
 };
 
+// Image Component
 const Image = () => (
     <img
         src="./images/a-clash-of-kings.jpg"
         alt="Game of Thrones book cover"
     ></img>
 );
-const Title = () => <h2>A Game of Thrones</h2>;
-const Author = () => <h4>George R. R. Martin</h4>;
 
+// Title Component
+const Title = () => <h2>A Game of Thrones</h2>;
+
+// Author Component
+const Author = () => (
+    <h4 style={{ color: "#8f806b", fontSize: "1rem", marginTop: "0.5rem" }}>
+        George R. R. Martin
+    </h4>
+);
+
+// Book Component
 function Book() {
     return (
         <article className="book">
