615ca1fbfc4b68cfe12824f4cdb1fd81f1f0d31a
jeffersonrolino <jefferson.rolino@gmail.com>
2024-06-27 17:12:19 -0300

    Lesson 34 and 35: Proper List and Key Prop

--- 03-fundamentals/src/index.js
+++ 03-fundamentals/src/index.js
@@ -4,11 +4,13 @@ import "./index.css";
 
 const books = [
     {
+        id: 1,
         title: "A Game of Thrones",
         author: "George R. R. Martin",
         image: "./images/a-game-of-thrones.jpg",
     },
     {
+        id: 2,
         title: "A Clash of Kings",
         author: "George R. R. Martin",
         image: "./images/a-clash-of-kings.jpg",
@@ -21,15 +23,20 @@ const BookList = () => {
         <div className="container">
             <h1>Fantasy Books</h1>
             <section className="book-list">
-                {names.map((name) => {
-                    return <h2>{name}</h2>;
+                {books.map((book) => {
+                    return (
+                        <Book
+                            key={book.id}
+                            image={book.image}
+                            title={book.title}
+                            author={book.author}
+                        />
+                    );
                 })}
             </section>
         </div>
     );
 };
-
-const names = ["John", "Peter", "Mary", "Tony"];
 
 // Book Component
 function Book({ image, title, author }) {
