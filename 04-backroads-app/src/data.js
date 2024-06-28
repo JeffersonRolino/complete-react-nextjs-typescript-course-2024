b853b715c297618c181adbc389c7cd8791b38941
jeffersonrolino <jefferson.rolino@gmail.com>
2024-06-28 20:21:01 -0300

    Lesson 65: Services Component

--- 04-backroads-app/src/data.js
+++ 04-backroads-app/src/data.js
@@ -10,3 +10,27 @@ export const socialLinks = [
   { id: 2, href: "https://www.facebook.com", icon: "fab fa-twitter" },
   { id: 3, href: "https://www.squarespace.com", icon: "fab fa-squarespace" },
 ];
+
+export const services = [
+  {
+    id: 1,
+    icon: "fas fa-wallet fa-fw",
+    title: "saving money",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+  {
+    id: 2,
+    icon: "fas fa-tree fa-fw",
+    title: "endless hiking",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+  {
+    id: 3,
+    icon: "fas fa-socks fa-fw",
+    title: "amazing comfort",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+];
