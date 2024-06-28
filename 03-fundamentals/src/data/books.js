21889a36a1431b1bff7d09a19edfb3b52ef8821e
jeffersonrolino <jefferson.rolino@gmail.com>
2024-06-28 16:05:20 -0300

    Lesson 47: Local Images (src folder)

--- 03-fundamentals/src/data/books.js
+++ 03-fundamentals/src/data/books.js
@@ -1,15 +1,46 @@
+import aGameOfThronesCover from "../images/a-game-of-thrones.jpg";
+import aClashOfKingsCover from "../images/a-clash-of-kings.jpg";
+import aStormOfSwordsCover from "../images/a-storm-of-swords.jpg";
+import aFeastForCrowsCover from "../images/a-feast-for-crows.jpg";
+import aDanceWithDragonsCover from "../images/a-dance-with-dragons.jpg";
+import fireAndBloodCover from "../images/fire-and-blood.jpg";
+
 const books = [
     {
         id: 1,
         title: "A Game of Thrones",
         author: "George R. R. Martin",
-        image: "./images/a-game-of-thrones.jpg",
+        image: aGameOfThronesCover,
     },
     {
         id: 2,
         title: "A Clash of Kings",
         author: "George R. R. Martin",
-        image: "./images/a-clash-of-kings.jpg",
+        image: aClashOfKingsCover,
+    },
+    {
+        id: 3,
+        title: "A Storm of Swords",
+        author: "George R. R. Martin",
+        image: aStormOfSwordsCover,
+    },
+    {
+        id: 4,
+        title: "A Feast for Crows",
+        author: "George R. R. Martin",
+        image: aFeastForCrowsCover,
+    },
+    {
+        id: 5,
+        title: "A Dance with Dragons",
+        author: "George R. R. Martin",
+        image: aDanceWithDragonsCover,
+    },
+    {
+        id: 6,
+        title: "Fire and Blood",
+        author: "George R. R. Martin",
+        image: fireAndBloodCover,
     },
 ];
 
