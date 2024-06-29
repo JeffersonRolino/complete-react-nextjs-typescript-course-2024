e87803d547206a1001c9fa86aa302d39842813d1
jeffersonrolino <jefferson.rolino@gmail.com>
2024-06-28 22:29:23 -0300

    Lesson 66: Tours Component

--- 04-backroads-app/src/data.js
+++ 04-backroads-app/src/data.js
@@ -1,3 +1,8 @@
+import tour1Image from "./images/tour-1.jpeg";
+import tour2Image from "./images/tour-2.jpeg";
+import tour3Image from "./images/tour-3.jpeg";
+import tour4Image from "./images/tour-4.jpeg";
+
 export const pageLinks = [
   { id: 1, href: "#home", text: "home" },
   { id: 2, href: "#about", text: "about" },
@@ -32,5 +37,52 @@ export const services = [
     title: "amazing comfort",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+];
+
+export const tours = [
+  {
+    id: 1,
+    image: tour1Image,
+    date: "august 26th, 2020",
+    title: "Tibet Adventure",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "China",
+    days: "6 days",
+    price: "$2100",
+  },
+  {
+    id: 2,
+    image: tour2Image,
+    date: "october 1th, 2020",
+    title: "Best of Java",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "Indonesia",
+    days: "11 days",
+    price: "$1400",
+  },
+  {
+    id: 3,
+    image: tour3Image,
+    date: "december 5th, 2019",
+    title: "Explore Hong Kong",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "China",
+    days: "8 days",
+    price: "$5000",
+  },
+  {
+    id: 4,
+    image: tour4Image,
+    date: "october 1th, 2020",
+    title: "Kenya Highlights",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "Kenya",
+    days: "20 days",
+    price: "$3300",
   },
 ];
