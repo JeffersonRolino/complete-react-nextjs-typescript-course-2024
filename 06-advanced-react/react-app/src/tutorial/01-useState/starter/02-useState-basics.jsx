061d3e28cd6bd704b01c5dfb048802d5d7355682
jeffersonrolino <jefferson.rolino@gmail.com>
2024-06-29 11:27:06 -0300

    Lesson 80: UseState Fundamentals

--- 06-advanced-react/react-app/src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ 06-advanced-react/react-app/src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -1,5 +1,20 @@
+import { useState } from "react";
+
 const UseStateBasics = () => {
-  return <h2>useState basics</h2>;
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <h2>You Clicked {count} Times...</h2>
+      <button className="btn" onClick={handleClick}>
+        Click Me
+      </button>
+    </div>
+  );
 };
 
 export default UseStateBasics;
