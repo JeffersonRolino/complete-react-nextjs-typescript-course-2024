c8493d004abbabd4a69d5af9a9a1c0ec9e6e6ac1
jeffersonrolino <jefferson.rolino@gmail.com>
2024-06-29 12:25:25 -0300

    Lessons 83, 84 and 85: useState Challenge and useState Extra info

--- 06-advanced-react/react-app/src/tutorial/01-useState/starter/03-useState-array.jsx
+++ 06-advanced-react/react-app/src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,39 @@
+import { useState } from "react";
+import { data } from "../../../data";
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data);
+
+  const removeItem = (id) => {
+    setPeople(
+      people.filter((person) => {
+        return person.id !== id;
+      })
+    );
+  };
+
+  const clearAllItems = () => {
+    setPeople([]);
+  };
+
+  return (
+    <div className="people-container">
+      <h1>Heroes</h1>
+      {people.map((person) => {
+        return (
+          <div key={person.id} className="person-container">
+            <h4>{person.name}</h4>
+            <button className="btn" onClick={() => removeItem(person.id)}>
+              Remove
+            </button>
+          </div>
+        );
+      })}
+      <button className="clear-btn" onClick={clearAllItems}>
+        Clear List
+      </button>
+    </div>
+  );
 };
 
 export default UseStateArray;
