487aa34f0a69f9ff77f6a904534ea4e940983c27
jeffersonrolino <jefferson.rolino@gmail.com>
2024-06-30 17:46:56 -0300

    Lesson 93: Code Example

--- 06-advanced-react/src/tutorial/02-useEffect/starter/01-code-example.jsx
+++ 06-advanced-react/src/tutorial/02-useEffect/starter/01-code-example.jsx
@@ -1,12 +1,23 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const CodeExample = () => {
   const [value, setValue] = useState(0);
 
+  const sayHello = () => {
+    console.log("The component was rendered...");
+    // So be careful, because if you change the state here
+    // you will have a infinite loop: Error: To many renders...
+    // setValue(value + 1);
+  };
+
+  // This function will be called every time
+  // the component renders and re-renders...
+  sayHello();
+
   return (
     <div>
       <h1>value : {value}</h1>
-      <button className='btn' onClick={() => setValue(value + 1)}>
+      <button className="btn" onClick={() => setValue(value + 1)}>
         click me
       </button>
     </div>
