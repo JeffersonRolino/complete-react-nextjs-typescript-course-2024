4f436286fff700d375bc7b4c7278cf1c14ec7f80
jeffersonrolino <jefferson.rolino@gmail.com>
2024-06-30 10:50:56 -0300

    Lesson 89: UseState Set Function Gotcha

--- 06-advanced-react/react-app/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ 06-advanced-react/react-app/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,22 @@
+import { useState } from "react";
+
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+  const [counter, setCounter] = useState(0);
+
+  const handleClick = () => {
+    setCounter(counter + 1);
+    console.log(counter);
+  };
+
+  return (
+    <div>
+      <h2>useState Gotcha</h2>
+      <h5>You clicked {counter} times...</h5>
+      <button className="btn" onClick={handleClick}>
+        Click Me
+      </button>
+    </div>
+  );
 };
 
 export default UseStateGotcha;
