876e692f8e12d7e967d37d2c7d3e82a17066a499
jeffersonrolino <jefferson.rolino@gmail.com>
2024-07-01 14:29:07 -0300

    Lesson 110: Toggle Challenge

--- 06-advanced-react/src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
+++ 06-advanced-react/src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
@@ -1,5 +1,21 @@
+import { useState } from "react";
+
 const ToggleChallenge = () => {
-  return <h2>toggle challenge</h2>;
+  const [showAlert, setShowAlert] = useState(false);
+
+  return (
+    <div className="container">
+      <h1 style={{ marginBottom: "2rem" }}>Toggle Challenge</h1>
+      {showAlert && <Alert />}
+      <button className="btn" onClick={() => setShowAlert(!showAlert)}>
+        Toogle
+      </button>
+    </div>
+  );
+};
+
+const Alert = () => {
+  return <div className="alert alert-danger">This is a Alert!</div>;
 };
 
 export default ToggleChallenge;
