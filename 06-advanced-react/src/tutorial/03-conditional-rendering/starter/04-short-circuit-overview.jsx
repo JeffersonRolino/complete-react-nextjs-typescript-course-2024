c4c2f53e6d4850b209360846acd14d05d2864e22
jeffersonrolino <jefferson.rolino@gmail.com>
2024-07-01 11:38:09 -0300

    Lesson 107: Short Circuit - Overview

--- 06-advanced-react/src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
+++ 06-advanced-react/src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
@@ -1,6 +1,21 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const ShortCircuitOverview = () => {
-  return <h2>short circuit overview</h2>;
+  const [text, setText] = useState("");
+  const [name, setName] = useState("Peter");
+
+  return (
+    <div>
+      <h1>Short Circuit Overview</h1>
+      <br />
+      <h2>|| Operator</h2>
+      <h4>Falsy OR: {text || "Render Successful"}</h4>
+      <h4>Falsy OR: {name || "Render Successful"}</h4>
+      <br />
+      <h2>&& Operator</h2>
+      <h4>Truthy &&: {text && "Render Successful"}</h4>
+      <h4>Truthy &&: {name && "Render Successful"}</h4>
+    </div>
+  );
 };
 export default ShortCircuitOverview;
