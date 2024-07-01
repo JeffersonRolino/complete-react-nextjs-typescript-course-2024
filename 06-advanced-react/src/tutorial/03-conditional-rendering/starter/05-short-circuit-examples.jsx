a5ee8768d782bc859ea583ea4ce45fb6466572e6
jeffersonrolino <jefferson.rolino@gmail.com>
2024-07-01 12:03:52 -0300

    Lesson 109: Ternary Operator

--- 06-advanced-react/src/tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
+++ 06-advanced-react/src/tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
@@ -5,8 +5,8 @@ const ShortCircuitExamples = () => {
   const [text, setText] = useState("");
   // truthy
   const [name, setName] = useState("susan");
-  const [user, setUser] = useState({ name: "john" });
-  const [isEditing, setIsEditing] = useState(false);
+  const [user, setUser] = useState({ name: "John Snow" });
+  const [isEditing, setIsEditing] = useState(true);
 
   return (
     <div>
@@ -18,6 +18,9 @@ const ShortCircuitExamples = () => {
         </div>
       )} */}
       {user && <SomeComponent name={user.name} />}
+      <h2 style={{ margin: "2rem 0" }}>Ternary Operator</h2>
+      <button className="btn">{isEditing ? "edit" : "add"}</button>
+      {user ? <Logged name={user.name} /> : <NotLogged />}
     </div>
   );
 };
@@ -27,6 +30,22 @@ const SomeComponent = ({ name }) => {
     <div>
       <h2>Hi my name is</h2>
       <h2>{name}</h2>
+    </div>
+  );
+};
+
+const Logged = ({ name }) => {
+  return (
+    <div>
+      <h4 style={{ marginTop: "2rem" }}>Hello {name}</h4>
+    </div>
+  );
+};
+
+const NotLogged = () => {
+  return (
+    <div>
+      <h4 style={{ marginTop: "2rem" }}>You need to login...</h4>
     </div>
   );
 };
