0194cdbab19ace5cfb5d249cac831f10a5e23e35
jeffersonrolino <jefferson.rolino@gmail.com>
2024-07-01 15:10:21 -0300

    Lesson 112: useEffect Cleanup Function - Setup/Challenge

--- 06-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ 06-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,24 @@
+import { useEffect, useState } from "react";
+
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <div>
+      {toggle && <FirstComponent />}
+      <button className="btn" onClick={() => setToggle(!toggle)}>
+        Toggle
+      </button>
+    </div>
+  );
+};
+
+const FirstComponent = () => {
+  useEffect(() => {
+    console.log("hmm, this is interesting...");
+  }, []);
+
+  return <h3>First Component</h3>;
 };
 
 export default CleanupFunction;
