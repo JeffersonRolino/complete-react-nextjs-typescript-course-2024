97c48d3370ed897fd80c239b46d077b6a68580d1
jeffersonrolino <jefferson.rolino@gmail.com>
2024-07-03 11:03:07 -0300

    Lesson 123: Forms - Setup

--- 06-advanced-react/src/App.jsx
+++ 06-advanced-react/src/App.jsx
@@ -1,10 +1,9 @@
-import List from "./tutorial/05-leverage-javascript/starter/List";
-
+import Starter from "./tutorial/06-forms/starter/01-controlled-inputs";
 function App() {
   return (
     <div className="container">
       <h1>Leverage Javascript</h1>
-      <List />
+      <Starter />
     </div>
   );
 }
