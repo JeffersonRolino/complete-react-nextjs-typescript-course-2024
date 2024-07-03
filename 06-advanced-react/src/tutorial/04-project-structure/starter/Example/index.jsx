efd08024c6946c67af747361cb348f9530993eac
jeffersonrolino <jefferson.rolino@gmail.com>
2024-07-03 09:36:14 -0300

    Lesson 119: Project Structure - Export Group

---
+++ 06-advanced-react/src/tutorial/04-project-structure/starter/Example/index.jsx
@@ -0,0 +1,14 @@
+import FirstComponent from "./FirstComponent";
+import SecondComponent from "./SecondComponent";
+
+function Example() {
+  return (
+    <div>
+      <h1>Example Component</h1>
+      <FirstComponent />
+      <SecondComponent />
+    </div>
+  );
+}
+
+export default Example;
