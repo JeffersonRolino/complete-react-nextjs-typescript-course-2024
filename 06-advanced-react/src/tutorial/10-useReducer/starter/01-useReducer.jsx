f851ecbb86180a596bdce3ce6c431a98b450a699
jeffersonrolino <jefferson.rolino@gmail.com>
2024-07-05 12:18:00 -0300

    Lesson 148: Reducer Basics

--- 06-advanced-react/src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ 06-advanced-react/src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -1,11 +1,15 @@
-import { useState, useReducer } from "react";
+import { useReducer } from "react";
 import { data } from "../../../data";
 
 const defaultState = {
   people: data,
 };
 
-const reducer = () => {};
+const reducer = (state, action) => {
+  if (action.type === "CLEAR_LIST") {
+    return { ...state, people: [] };
+  }
+};
 
 const ReducerBasics = () => {
   const [state, dispatch] = useReducer(reducer, defaultState);
@@ -15,6 +19,10 @@ const ReducerBasics = () => {
   const removeItem = (id) => {
     // let newPeople = people.filter((person) => person.id !== id);
     // setPeople(newPeople);
+  };
+
+  const clearList = () => {
+    dispatch({ type: "CLEAR_LIST" });
   };
 
   const resetList = () => {
@@ -36,7 +44,7 @@ const ReducerBasics = () => {
         <button
           className="btn"
           style={{ marginTop: "2rem", backgroundColor: "#D21942" }}
-          onClick={() => setPeople([])}
+          onClick={clearList}
         >
           clear items
         </button>
