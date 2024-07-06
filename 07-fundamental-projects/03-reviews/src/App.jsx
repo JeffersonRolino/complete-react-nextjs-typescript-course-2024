f5762cd6a32d599f8461a4ea71ad760657491e5f
jeffersonrolino <jefferson.rolino@gmail.com>
2024-07-06 12:06:13 -0300

    Lesson 186: Reviews - Prev and Next

--- 07-fundamental-projects/03-reviews/src/App.jsx
+++ 07-fundamental-projects/03-reviews/src/App.jsx
@@ -6,7 +6,20 @@ const App = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
 
-  console.log(name);
+  const nextPerson = () => {
+    setIndex((currentIndex) => {
+      const newIndex = currentIndex + 1;
+      return newIndex;
+    });
+  };
+
+  const previousPerson = () => {
+    setIndex((currentIndex) => {
+      const newIndex = currentIndex - 1;
+      return newIndex;
+    });
+  };
+
   return (
     <main>
       <article className="review">
@@ -19,6 +32,15 @@ const App = () => {
         <h4 className="author">{name}</h4>
         <p className="job">{job}</p>
         <p className="info">{text}</p>
+
+        <div className="btn-container">
+          <button className="prev-btn" onClick={previousPerson}>
+            <FaChevronLeft />
+          </button>
+          <button className="next-btn" onClick={nextPerson}>
+            <FaChevronRight />
+          </button>
+        </div>
       </article>
     </main>
   );
