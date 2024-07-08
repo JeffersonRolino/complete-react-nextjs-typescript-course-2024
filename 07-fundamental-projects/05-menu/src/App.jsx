bd2338bf4ec7ffb746814d0874c599d5521c2af4
jeffersonrolino <jefferson.rolino@gmail.com>
2024-07-07 23:31:07 -0300

    Lesson 199: Menu - Unique Categories

--- 07-fundamental-projects/05-menu/src/App.jsx
+++ 07-fundamental-projects/05-menu/src/App.jsx
@@ -3,8 +3,13 @@ import menu from "./data";
 import Title from "./components/Title";
 import Menu from "./components/Menu";
 
+const allCategories = ["all", ...new Set(menu.map((item) => item.category))];
+
 const App = () => {
   const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState(allCategories);
+
+  console.log(categories);
 
   return (
     <main>
