156d60c1e8db8646b176a0813b6b428069f3d3f0
jeffersonrolino <jefferson.rolino@gmail.com>
2024-07-07 23:35:21 -0300

    Lesson 200: Menu - Display Categories

--- 07-fundamental-projects/05-menu/src/App.jsx
+++ 07-fundamental-projects/05-menu/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import menu from "./data";
 import Title from "./components/Title";
 import Menu from "./components/Menu";
+import Categories from "./components/Categories";
 
 const allCategories = ["all", ...new Set(menu.map((item) => item.category))];
 
@@ -9,12 +10,11 @@ const App = () => {
   const [menuItems, setMenuItems] = useState(menu);
   const [categories, setCategories] = useState(allCategories);
 
-  console.log(categories);
-
   return (
     <main>
       <section className="menu">
         <Title text={"Our Menu"} />
+        <Categories categories={categories} />
         <Menu items={menuItems} />
       </section>
     </main>
