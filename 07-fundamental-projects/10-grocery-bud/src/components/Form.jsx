c6e1974dfc288f0ea6392391b547e9aa78ca245d
jeffersonrolino <jefferson.rolino@gmail.com>
2024-07-10 19:19:11 -0300

    Lesson 234: Grocery Bud - Form Component

---
+++ 07-fundamental-projects/10-grocery-bud/src/components/Form.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+function Form() {
+  const [newItemName, setNewItemName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(newItemName);
+    setNewItemName("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h4>Grocery Bud</h4>
+      <div className="form-control">
+        <input
+          type="text"
+          className="form-input"
+          value={newItemName}
+          onChange={(e) => setNewItemName(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          Add Item
+        </button>
+      </div>
+    </form>
+  );
+}
+
+export default Form;
