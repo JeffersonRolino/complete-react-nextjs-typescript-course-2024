430897bfdd5f32551a099297bdb4cf29ded9eaf2
jeffersonrolino <jefferson.rolino@gmail.com>
2024-07-12 17:01:14 -0300

    Lesson 255: Sidebar / Modal - Modal Component

--- 07-fundamental-projects/12-sidebar/src/components/Modal.jsx
+++ 07-fundamental-projects/12-sidebar/src/components/Modal.jsx
@@ -1,7 +1,17 @@
+import { FaTimes } from "react-icons/fa";
+import { useGlobalContext } from "../Context";
+
 function Modal() {
+  const { isModalOpen, closeModal } = useGlobalContext();
+
   return (
-    <div>
-      <h2>Modal</h2>
+    <div className={isModalOpen ? "modal-overlay show-modal" : "modal-overlay"}>
+      <div className="modal-container">
+        <h3>Modal content</h3>
+        <button className="close-modal-btn" onClick={closeModal}>
+          <FaTimes />
+        </button>
+      </div>
     </div>
   );
 }
