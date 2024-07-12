df6528cfa0c220e07eaab8f2a5bb25ab3aa19a26
jeffersonrolino <jefferson.rolino@gmail.com>
2024-07-12 01:17:47 -0300

    Lesson 253: Sidebar / Modal - Global Context

--- 07-fundamental-projects/12-sidebar/src/components/Home.jsx
+++ 07-fundamental-projects/12-sidebar/src/components/Home.jsx
@@ -1,4 +1,10 @@
+import { useGlobalContext } from "../Context";
+
 function Home() {
+  const { openSidebar, openModal } = useGlobalContext();
+
+  console.log(openSidebar, openModal);
+
   return (
     <div>
       <h2>Home</h2>
