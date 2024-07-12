17f7e476b5dee4223ad2b103bbf179235a1fcf93
jeffersonrolino <jefferson.rolino@gmail.com>
2024-07-12 18:58:01 -0300

    Lesson 256: Sidebar / Modal - Sidebar Component JSX

--- 07-fundamental-projects/12-sidebar/src/components/Sidebar.jsx
+++ 07-fundamental-projects/12-sidebar/src/components/Sidebar.jsx
@@ -1,8 +1,44 @@
+import logo from "../logo.svg";
+import { social, links } from "../data";
+import { FaTimes } from "react-icons/fa";
+import { useGlobalContext } from "../Context";
+
 function Sidebar() {
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
+
   return (
-    <div>
-      <h2>Sidebar</h2>
-    </div>
+    <aside className="sidebar">
+      <div className="sidebar-header">
+        <img src={logo} alt="logo" className="logo" />
+        <button className="close-btn" onClick={closeSidebar}>
+          <FaTimes />
+        </button>
+      </div>
+
+      <ul className="links">
+        {links.map((link) => {
+          const { id, url, text, icon } = link;
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon} {text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+
+      <ul className="social-links">
+        {social.map((link) => {
+          const { id, url, icon } = link;
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </aside>
   );
 }
 
