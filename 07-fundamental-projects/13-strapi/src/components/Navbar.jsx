e344057316854dd0525fc35c35a719c8ad170924
jeffersonrolino <jefferson.rolino@gmail.com>
2024-07-13 18:14:08 -0300

    Lesson 263: Strapi - Navbar CSS

--- 07-fundamental-projects/13-strapi/src/components/Navbar.jsx
+++ 07-fundamental-projects/13-strapi/src/components/Navbar.jsx
@@ -5,13 +5,15 @@ function Navbar() {
   const { openSidebar } = useGlobalContext();
 
   return (
-    <div className="nav-center">
-      <h3 className="logo">strapi</h3>
-      <button className="toggle-btn" onClick={openSidebar}>
-        <FaBars />
-      </button>
-      {/* nav links later */}
-    </div>
+    <nav>
+      <div className="nav-center">
+        <h3 className="logo">strapi</h3>
+        <button className="toggle-btn" onClick={openSidebar}>
+          <FaBars />
+        </button>
+        {/* nav links later */}
+      </div>
+    </nav>
   );
 }
 
