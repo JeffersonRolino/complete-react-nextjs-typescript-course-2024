4e0761298deefd9a43cc4f53da9107f872935522
jeffersonrolino <jefferson.rolino@gmail.com>
2024-07-13 23:13:34 -0300

    Lesson 268: Strapi - PageId Logic

--- 07-fundamental-projects/13-strapi/src/components/NavLinks.jsx
+++ 07-fundamental-projects/13-strapi/src/components/NavLinks.jsx
@@ -1,12 +1,19 @@
+import { useGlobalContext } from "../context";
 import sublinks from "../data";
 
 function NavLinks() {
+  const { setPageId } = useGlobalContext();
+
   return (
     <div className="nav-links">
       {sublinks.map((item) => {
         const { pageId, page } = item;
         return (
-          <button key={pageId} className="nav-link">
+          <button
+            key={pageId}
+            className="nav-link"
+            onMouseEnter={() => setPageId(pageId)}
+          >
             {page}
           </button>
         );
