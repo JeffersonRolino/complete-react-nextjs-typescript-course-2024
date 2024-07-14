3800771f2709080e099d29a3086934722a5afd5f
jeffersonrolino <jefferson.rolino@gmail.com>
2024-07-13 23:22:43 -0300

    Lesson 269: Strapi - Submenu JSX

--- 07-fundamental-projects/13-strapi/src/components/Submenu.jsx
+++ 07-fundamental-projects/13-strapi/src/components/Submenu.jsx
@@ -1,7 +1,32 @@
+import { useGlobalContext } from "../context";
+import sublinks from "../data";
+
 function Submenu() {
+  const { pageId } = useGlobalContext();
+  const currentPage = sublinks.find((item) => item.pageId === pageId);
+
+  console.log(currentPage);
+
   return (
-    <div>
-      <h2>Submenu</h2>
+    <div className="submenu">
+      <h5>{currentPage?.page}</h5>
+      <div
+        className="submenu-links"
+        style={{
+          display: "grid",
+          gridTemplateColumns:
+            currentPage?.links?.length > 3 ? "1fr 1fr" : "1fr",
+        }}
+      >
+        {currentPage?.links?.map((link) => {
+          const { id, url, label, icon } = link;
+          return (
+            <a key={id} href={url}>
+              {icon} {label}
+            </a>
+          );
+        })}
+      </div>
     </div>
   );
 }
