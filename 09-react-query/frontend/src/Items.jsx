1866dd401abd5e284b8e83da7b7f44b21ce39ee4
jeffersonrolino <jefferson.rolino@gmail.com>
2024-07-15 23:55:47 -0300

    Lesson 309: React Query - First Query

--- 09-react-query/frontend/src/Items.jsx
+++ 09-react-query/frontend/src/Items.jsx
@@ -1,7 +1,16 @@
-import SingleItem from './SingleItem';
+import { useQuery } from "@tanstack/react-query";
+import SingleItem from "./SingleItem";
+import customFetch from "./utils/customInstance";
+
 const Items = ({ items }) => {
+  const result = useQuery({
+    queryKey: ["tasks"],
+    queryFn: () => customFetch.get("/"),
+  });
+  console.log(result);
+
   return (
-    <div className='items'>
+    <div className="items">
       {items.map((item) => {
         return <SingleItem key={item.id} item={item} />;
       })}
