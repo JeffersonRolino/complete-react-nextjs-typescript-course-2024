c87a31f236ff98073be25517090ff4f969436bba
jeffersonrolino <jefferson.rolino@gmail.com>
2024-07-17 17:56:35 -0300

    Lesson 334: Unsplash Project - Setup Query

--- 10-unsplash-project/src/components/Gallery.jsx
+++ 10-unsplash-project/src/components/Gallery.jsx
@@ -1,4 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const API_URL = import.meta.env.VITE_API_URL;
+const KEY = import.meta.env.VITE_API_KEY;
+const url = `${API_URL}?client_id=${KEY}&query=cat`;
+
 function Gallery() {
+  const response = useQuery({
+    queryKey: ["images"],
+    queryFn: async () => {
+      const result = await axios.get(url);
+
+      return result.data;
+    },
+  });
+
+  console.log(response);
+
   return (
     <div>
       <h2>Gallery</h2>
