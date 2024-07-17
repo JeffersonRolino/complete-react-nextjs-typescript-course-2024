41efce19a02dc05e617d125644925c69cc018233
jeffersonrolino <jefferson.rolino@gmail.com>
2024-07-16 23:29:43 -0300

    Lesson 325: Unsplash Project - Dark Theme: Initial Setup

--- 10-unsplash-project/src/components/ThemeToggle.jsx
+++ 10-unsplash-project/src/components/ThemeToggle.jsx
@@ -1,8 +1,19 @@
+import { BsFillSunFill, BsFillMoonFill } from "react-icons/bs";
+import { useGlobalContext } from "../context";
+
 function ThemeToggle() {
+  const { isDarkTheme, toggleDarkTheme } = useGlobalContext();
+
   return (
-    <div>
-      <h2>Theme Toggle</h2>
-    </div>
+    <section className="toggle-container">
+      <button className="dark-toggle" onClick={toggleDarkTheme}>
+        {isDarkTheme ? (
+          <BsFillMoonFill className="toggle-icon" />
+        ) : (
+          <BsFillSunFill className="toggle-icon" />
+        )}
+      </button>
+    </section>
   );
 }
 
