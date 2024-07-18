75ecc5b52bee44b52cc14cb1cee2f2c6c3e3de3a
jeffersonrolino <jefferson.rolino@gmail.com>
2024-07-18 20:37:06 -0300

    Lesson 348: Contentful CMS Project - Hero Component

---
+++ 11-contentful-cms/src/components/Hero.jsx
@@ -0,0 +1,24 @@
+import heroImg from "../assets/hero.svg";
+
+function Hero() {
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <div className="hero-title">
+          <h1>Contentful CMS</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quos
+            debitis enim porro eaque itaque laborum, suscipit, voluptatum
+            voluptate quasi modi commodi reprehenderit fugiat illo optio ipsa
+            molestiae fuga quibusdam.
+          </p>
+        </div>
+        <div className="img-container">
+          <img src={heroImg} alt="woman and the browser" />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
