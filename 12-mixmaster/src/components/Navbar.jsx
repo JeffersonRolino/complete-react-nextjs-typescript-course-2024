e1be6041b5a2a0990d6d63f8f92e87fff43cf306
jeffersonrolino <jefferson.rolino@gmail.com>
2024-07-20 20:37:25 -0300

    Lesson 373: MixMaster - Styled Components - Wrapper

--- 12-mixmaster/src/components/Navbar.jsx
+++ 12-mixmaster/src/components/Navbar.jsx
@@ -1,18 +1,10 @@
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
-const StyleBtn = styled.button`
-  background-color: red;
-  color: white;
-  font-size: 2rem;
-  padding: 1rem;
-`;
-
 function Navbar() {
   return (
-    <nav>
+    <Wrapper>
       <div className="nav-center">
-        <StyleBtn>style btn</StyleBtn>
         <span className="logo">MixMaster</span>
         <div className="nav-links">
           <NavLink to="/" className="nav-link">
@@ -26,8 +18,13 @@ function Navbar() {
           </NavLink>
         </div>
       </div>
-    </nav>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.nav`
+  background-color: red;
+  color: white;
+`;
 
 export default Navbar;
