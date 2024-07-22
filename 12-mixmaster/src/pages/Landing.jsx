c80600aaef38d06e8bfeaaa2597a1c98b5a5917c
jeffersonrolino <jefferson.rolino@gmail.com>
2024-07-21 22:21:26 -0300

    Lesson 380: MixMaster - Loader - Landing Page

--- 12-mixmaster/src/pages/Landing.jsx
+++ 12-mixmaster/src/pages/Landing.jsx
@@ -1,4 +1,13 @@
+import { useLoaderData } from "react-router-dom";
+
+export const loader = async () => {
+  return "something";
+};
+
 function Landing() {
+  const data = useLoaderData();
+  console.log(data);
+
   return (
     <div>
       <h1>Landing</h1>
