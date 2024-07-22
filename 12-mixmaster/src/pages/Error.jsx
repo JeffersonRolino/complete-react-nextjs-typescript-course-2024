27008e146453bcf633b08e892574d5f88ea36143
jeffersonrolino <jefferson.rolino@gmail.com>
2024-07-21 21:58:52 -0300

    Lesson 377: MixMaster - Error Page

--- 12-mixmaster/src/pages/Error.jsx
+++ 12-mixmaster/src/pages/Error.jsx
@@ -1,9 +1,34 @@
+import styled from "styled-components";
+import { Link, useRouteError } from "react-router-dom";
+import img from "../assets/not-found.svg";
+
 function Error() {
+  const error = useRouteError();
+  console.log(error);
+
+  if (error.status === 404) {
+    return (
+      <Wrapper>
+        <img src={img} alt="not found" />
+        <h3>Ohh!</h3>
+        <p>Sorry, we can't seem to find the page you are looking for...</p>
+        <Link to="/">back home</Link>
+      </Wrapper>
+    );
+  }
+
   return (
-    <div>
-      <h1>Error</h1>
-    </div>
+    <Wrapper>
+      <h3>Something went wrong</h3>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  h1 {
+    color: var(--grey-500);
+    margin-top: 2rem;
+  }
+`;
 
 export default Error;
