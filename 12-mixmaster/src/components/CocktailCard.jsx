5b9129d1d4f8cb90795fed066dfc1596c7203698
jeffersonrolino <jefferson.rolino@gmail.com>
2024-07-21 23:22:53 -0300

    Lesson 385: MixMaster - Cocktail Card

--- 12-mixmaster/src/components/CocktailCard.jsx
+++ 12-mixmaster/src/components/CocktailCard.jsx
@@ -1,9 +1,24 @@
-function CocktailCard() {
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+function CocktailCard({ id, image, name, info, glass }) {
   return (
-    <div>
-      <h3>Cocktail Card</h3>
-    </div>
+    <Wrapper>
+      <div className="img-container">
+        <img src={image} alt={name} className="img" />
+      </div>
+      <div className="footer">
+        <h4>{name}</h4>
+        <h4>{glass}</h4>
+        <p>{info}</p>
+        <Link to={`/cocktail/${id}`} className="btn">
+          details
+        </Link>
+      </div>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div``;
 
 export default CocktailCard;
