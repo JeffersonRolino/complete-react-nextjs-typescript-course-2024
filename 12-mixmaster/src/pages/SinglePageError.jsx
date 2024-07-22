5e5d85ab77fb30889a6c8c77485021ea668e40c8
jeffersonrolino <jefferson.rolino@gmail.com>
2024-07-21 23:03:21 -0300

    Lesson 383: MixMaster - Single Page Error

---
+++ 12-mixmaster/src/pages/SinglePageError.jsx
@@ -0,0 +1,13 @@
+import { useRouteError } from "react-router-dom";
+
+function SinglePageError() {
+  const error = useRouteError();
+
+  return (
+    <div>
+      <h2>{error.message}</h2>
+    </div>
+  );
+}
+
+export default SinglePageError;
