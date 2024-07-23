dc9a8b9e31cc0c0efa3f5d29a91087eee2ac926e
jeffersonrolino <jefferson.rolino@gmail.com>
2024-07-22 22:00:03 -0300

    Lesson 389: MixMaster - Single Cocktail - Render

--- 12-mixmaster/src/pages/Cocktail.jsx
+++ 12-mixmaster/src/pages/Cocktail.jsx
@@ -1,5 +1,6 @@
 import { useLoaderData, Link } from "react-router-dom";
 import axios from "axios";
+import styled from "styled-components";
 
 const sigleCocktailSearchUrl =
   "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
@@ -14,11 +15,54 @@ export const loader = async ({ params }) => {
 function Cocktail() {
   const { id, data } = useLoaderData();
 
+  const singleDrink = data.drinks[0];
+
+  const {
+    strDrink: name,
+    strDrinkThumb: image,
+    strAlcoholic: info,
+    strCategory: category,
+    strGlass: glass,
+    strInstructions: instructions,
+  } = singleDrink;
+
   return (
-    <div>
-      <h1>Cocktail</h1>
-    </div>
+    <Wrapper>
+      <header>
+        <Link to="/" className="btn">
+          back home
+        </Link>
+        <h3>{name}</h3>
+      </header>
+      <div className="drink">
+        <img src={image} alt={name} className="img" />
+        <div className="drink-infor">
+          <p>
+            <span className="drink-data">name: </span>
+            {name}
+          </p>
+          <p>
+            <span className="drink-data">category: </span>
+            {category}
+          </p>
+          <p>
+            <span className="drink-data">info: </span>
+            {info}
+          </p>
+          <p>
+            <span className="drink-data">glass: </span>
+            {glass}
+          </p>
+          <p>
+            <span className="drink-data">instructions: </span>
+            {instructions}
+          </p>
+        </div>
+      </div>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div``;
 
 export default Cocktail;
