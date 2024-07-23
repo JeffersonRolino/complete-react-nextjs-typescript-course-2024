5ae43a2b2475e38fd4d1a6c0df3379af8a10f987
jeffersonrolino <jefferson.rolino@gmail.com>
2024-07-22 22:43:43 -0300

    Lesson 392: MixMaster - Single Cocktail CSS

--- 12-mixmaster/src/pages/Cocktail.jsx
+++ 12-mixmaster/src/pages/Cocktail.jsx
@@ -45,7 +45,7 @@ function Cocktail() {
       </header>
       <div className="drink">
         <img src={image} alt={name} className="img" />
-        <div className="drink-infor">
+        <div className="drink-info">
           <p>
             <span className="drink-data">name: </span>
             {name}
@@ -83,6 +83,55 @@ function Cocktail() {
   );
 }
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  header {
+    text-align: center;
+    margin-bottom: 3rem;
+    .btn {
+      margin-bottom: 1rem;
+    }
+  }
+
+  .img {
+    border-radius: var(--borderRadius);
+  }
+
+  .drink-info {
+    padding-top: 2rem;
+  }
+
+  .drink p {
+    font-weight: 700;
+    text-transform: capitalize;
+    line-height: 2;
+    margin-bottom: 1rem;
+  }
+
+  .drink-data {
+    margin-right: 0.5rem;
+    background: var(--primary-300);
+    padding: 0.25rem 0.5rem;
+    border-radius: var(--borderRadius);
+    letter-spacing: var(--letterSpacing);
+  }
+
+  .ing {
+    display: inline-block;
+    margin-right: 0.5rem;
+  }
+
+  @media (min-width: 992px) {
+    .drink {
+      display: grid;
+      grid-template-columns: 2fr 3fr;
+      gap: 3rem;
+      align-items: center;
+    }
+
+    .drink-info {
+      padding-top: 0;
+    }
+  }
+`;
 
 export default Cocktail;
