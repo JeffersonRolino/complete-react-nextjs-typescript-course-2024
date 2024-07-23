16de78fb3e36bda794a773b34e86327bcafda54b
jeffersonrolino <jefferson.rolino@gmail.com>
2024-07-23 19:13:54 -0300

    Lesson 399: MixMaster - Navigation State

--- 12-mixmaster/src/pages/Newsletter.jsx
+++ 12-mixmaster/src/pages/Newsletter.jsx
@@ -1,4 +1,4 @@
-import { Form, redirect } from "react-router-dom";
+import { Form, redirect, useNavigation } from "react-router-dom";
 import axios from "axios";
 import { toast } from "react-toastify";
 
@@ -20,6 +20,9 @@ export const action = async ({ request }) => {
 };
 
 function Newsletter() {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+
   return (
     <Form className="form" method="POST">
       <h4 style={{ textAlign: "center", marginBottom: "2rem" }}>
@@ -69,8 +72,9 @@ function Newsletter() {
         type="submit"
         className="btn btn-block"
         style={{ marginTop: "0.5rem" }}
+        disabled={isSubmitting}
       >
-        submit
+        {isSubmitting ? "submitting" : "submit"}
       </button>
     </Form>
   );
