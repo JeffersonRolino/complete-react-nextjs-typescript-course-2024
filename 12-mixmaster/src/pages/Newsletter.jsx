5d62477b9508b146d440ff07b48f94eddf7b29d4
jeffersonrolino <jefferson.rolino@gmail.com>
2024-07-22 23:17:12 -0300

    Lesson 396: MixMaster - Action Setup

--- 12-mixmaster/src/pages/Newsletter.jsx
+++ 12-mixmaster/src/pages/Newsletter.jsx
@@ -1,6 +1,16 @@
+import { Form } from "react-router-dom";
+
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  console.log(data);
+
+  return null;
+};
+
 function Newsletter() {
   return (
-    <form className="form" method="POST">
+    <Form className="form" method="POST">
       <h4 style={{ textAlign: "center", marginBottom: "2rem" }}>
         Our Newsletter
       </h4>
@@ -50,7 +60,7 @@ function Newsletter() {
       >
         submit
       </button>
-    </form>
+    </Form>
   );
 }
 
