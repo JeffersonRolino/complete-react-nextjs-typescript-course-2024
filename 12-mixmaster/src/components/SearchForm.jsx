0b278e0a5f1aa8a8df946c52344d3a0db60fda60
jeffersonrolino <jefferson.rolino@gmail.com>
2024-07-23 21:05:52 -0300

    Lesson 402: MixMaster - SearchForm CSS

--- 12-mixmaster/src/components/SearchForm.jsx
+++ 12-mixmaster/src/components/SearchForm.jsx
@@ -22,6 +22,20 @@ function SearchForm({ searchTerm }) {
   );
 }
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  margin-bottom: 6rem;
+  .form {
+    display: grid;
+    grid-template-columns: 1fr auto;
+  }
+  .form-input {
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0;
+  }
+  .btn {
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+  }
+`;
 
 export default SearchForm;
