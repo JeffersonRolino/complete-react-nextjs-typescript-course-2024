0ce88e647b8dbf5d9d19793e59316d6c834d9c25
jeffersonrolino <jefferson.rolino@gmail.com>
2024-07-30 16:54:10 -0300

    Lesson 448: Comfy Store - Route Structure

--- 15-comfy-store/src/pages/HomeLayout.jsx
+++ 15-comfy-store/src/pages/HomeLayout.jsx
@@ -1,8 +1,13 @@
+import { Outlet } from "react-router-dom";
+
 function HomeLayout() {
   return (
-    <div>
-      <h1 className="text-4xl">HomeLayout</h1>
-    </div>
+    <>
+      <nav>
+        <span className="text-4xl text-primary">Comfy</span>
+      </nav>
+      <Outlet />
+    </>
   );
 }
 
