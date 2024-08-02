d44c80664cb097eb20b41886fa3607a071bcdd60
jeffersonrolino <jefferson.rolino@gmail.com>
2024-08-02 10:30:49 -0300

    Lesson 469: Comfy Store - Single Product Colors

--- 15-comfy-store/src/pages/SingleProduct.jsx
+++ 15-comfy-store/src/pages/SingleProduct.jsx
@@ -14,6 +14,7 @@ function SingleProduct() {
     product.attributes;
 
   const dollarsAmount = formatPrice(price);
+  const [productColor, setProductColor] = useState(colors[0]);
 
   return (
     <section>
@@ -44,6 +45,27 @@ function SingleProduct() {
           </h4>
           <p className="mt-3 text-xl">{dollarsAmount}</p>
           <p className="mt-6 leading-8">{description}</p>
+          {/* COLORS */}
+          <div className="mt-6">
+            <h4 className="font-medium text-md tracking-wider capitalize">
+              colors
+            </h4>
+            <div className="mt-2">
+              {colors.map((color) => {
+                return (
+                  <button
+                    key={color}
+                    type="button"
+                    className={`badge w-6 h-6 mr-2 ${
+                      color === productColor && "border-2 border-secondary"
+                    }`}
+                    style={{ backgroundColor: color }}
+                    onClick={() => setProductColor(color)}
+                  ></button>
+                );
+              })}
+            </div>
+          </div>
         </div>
       </div>
     </section>
