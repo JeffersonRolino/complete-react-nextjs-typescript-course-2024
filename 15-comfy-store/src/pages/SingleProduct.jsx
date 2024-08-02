d11e1a5456f050cdf56e8b205c8e9c754ca3c2de
jeffersonrolino <jefferson.rolino@gmail.com>
2024-08-02 10:16:47 -0300

    Lesson 467: Comfy Store - Fetch Single Product

--- 15-comfy-store/src/pages/SingleProduct.jsx
+++ 15-comfy-store/src/pages/SingleProduct.jsx
@@ -1,4 +1,20 @@
+import { useLoaderData } from "react-router-dom";
+import { formatPrice, customFetch } from "../utils";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
+export const loader = async ({ params }) => {
+  const response = await customFetch.get(`/products/${params.id}`);
+  return { product: response.data.data };
+};
+
 function SingleProduct() {
+  const { product } = useLoaderData();
+  const { image, title, price, description, colors, company } =
+    product.attributes;
+
+  const dollarsAmount = formatPrice(price);
+
   return (
     <div>
       <h1 className="text-4xl">Single Product</h1>
