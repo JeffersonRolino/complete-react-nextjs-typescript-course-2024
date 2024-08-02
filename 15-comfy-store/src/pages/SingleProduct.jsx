86f5b919ec09183e99a270338ec0f335f0abe4de
jeffersonrolino <jefferson.rolino@gmail.com>
2024-08-02 10:24:14 -0300

    Lesson 468: Comfy Store - Render Single Product

--- 15-comfy-store/src/pages/SingleProduct.jsx
+++ 15-comfy-store/src/pages/SingleProduct.jsx
@@ -16,9 +16,37 @@ function SingleProduct() {
   const dollarsAmount = formatPrice(price);
 
   return (
-    <div>
-      <h1 className="text-4xl">Single Product</h1>
-    </div>
+    <section>
+      <div className="text-md breadcrumbs">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+        </ul>
+      </div>
+
+      {/* PRODUCT */}
+      <div className="mt-6 grid gap-y-8 lg:grid-cols-2 lg:gap-16">
+        {/* IMAGE */}
+        <img
+          src={image}
+          alt={title}
+          className="w-96 h-96 object-cover rounded-lg lg:w-full"
+        />
+        {/* PRODUCT INFO */}
+        <div>
+          <h1 className="capitalize text-3xl font-bold">{title}</h1>
+          <h4 className="text-xl text-neutral-content font-bold mt-2">
+            {company}
+          </h4>
+          <p className="mt-3 text-xl">{dollarsAmount}</p>
+          <p className="mt-6 leading-8">{description}</p>
+        </div>
+      </div>
+    </section>
   );
 }
 
