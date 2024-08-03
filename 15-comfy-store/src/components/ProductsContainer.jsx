f1142a6724a72f6a3aca3fc50c292fdbf48279e0
jeffersonrolino <jefferson.rolino@gmail.com>
2024-08-03 10:32:19 -0300

    Lesson 473: Comfy Store - Products List

--- 15-comfy-store/src/components/ProductsContainer.jsx
+++ 15-comfy-store/src/components/ProductsContainer.jsx
@@ -1,5 +1,13 @@
+import ProductsGrid from "./ProductsGrid";
+import ProductsList from "./ProductsList";
+
 function ProductsContainer() {
-  return <div>Products Container</div>;
+  return (
+    <>
+      <ProductsList />
+      <ProductsGrid />
+    </>
+  );
 }
 
 export default ProductsContainer;
