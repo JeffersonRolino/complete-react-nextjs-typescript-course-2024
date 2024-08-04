054d82b45cb8082dc16b1d59641f0f9f9f0cc35d
jeffersonrolino <jefferson.rolino@gmail.com>
2024-08-04 18:09:05 -0300

    Lesson 484: Comfy Store - Add To Cart Functionality

--- 15-comfy-store/src/pages/SingleProduct.jsx
+++ 15-comfy-store/src/pages/SingleProduct.jsx
@@ -2,6 +2,8 @@ import { useLoaderData } from "react-router-dom";
 import { formatPrice, customFetch, generateAmountOptions } from "../utils";
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addItem } from "../features/cart/cartSlice";
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const loader = async ({ params }) => {
@@ -20,6 +22,22 @@ function SingleProduct() {
 
   const handleAmount = (e) => {
     setAmount(parseInt(e.target.value));
+  };
+
+  const cartProduct = {
+    cartID: product.id + productColor,
+    productID: product.id,
+    image,
+    title,
+    price,
+    company,
+    productColor,
+  };
+
+  const dispatch = useDispatch();
+
+  const addToCart = () => {
+    dispatch(addItem({ product: cartProduct }));
   };
 
   return (
@@ -90,10 +108,7 @@ function SingleProduct() {
           </div>
           {/* CART BTN */}
           <div className="mt-10">
-            <button
-              className="btn btn-secondary btn-md"
-              onClick={() => console.log("added to bag")}
-            >
+            <button className="btn btn-secondary btn-md" onClick={addToCart}>
               Add to bag
             </button>
           </div>
